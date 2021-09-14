a9460d395f6a4b1cdb12a2288c171f203e8bc7df
Atif <atif@monotote.com>
2021-09-14 18:36:18 +0500

    Netflix build one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Row from "./Row";
+import requests from './request';
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="app">
+        <Nav />
+        <Banner />
+        <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+        <Row title="Action movies" fetchUrl={requests.fetchActionMovies}/>
+        <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies}/>
+        <Row title="Horror movies" fetchUrl={requests.fetchHorrorMovies}/>
+        <Row title="Romance movies" fetchUrl={requests.fetchRomanticMovies}/>
+        <Row title="Documentries" fetchUrl={requests.fetchDocumentaries}/>
+      </div>
   );
 }
 
